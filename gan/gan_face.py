0d51955b850a12011c5bfd6c0d0fcfa3e3866efe
jacky09299 <66111556+jacky09299@users.noreply.github.com>
2023-11-28 17:57:08 +0800

    Update gan_face.py

--- gan/gan_face.py
+++ gan/gan_face.py
@@ -1,20 +1,16 @@
 import os
-import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision import transforms
 import numpy as np
 import matplotlib.pyplot as plt
 import PIL.Image as Image
 import torchvision.utils as vutils
-import torchvision.datasets as dset
 import cv2
-import random
-
 # Root directory for dataset
-dataroot = "dataset\data"
+dataroot = "dataset\\data"
 
 # Number of workers for dataloader
 workers = 1
@@ -61,7 +57,7 @@ class CustomDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir,self.images[idx])
+        img_path = os.path.join(self.root_dir, self.images[idx])
         image = Image.open(img_path).convert('RGB')
 
         if self.transform:
@@ -69,52 +65,52 @@ class CustomDataset(Dataset):
 
         return image
 
-class CustomDataset2(Dataset): # 繼承Dataset
-    def __init__(self, data_dir, img_size, num_classes): # 定義物件時，會執行的初始化函式
-        self.data_dir = data_dir # 設置訓練資料的位置
-        self.img_size = img_size # 設置圖片大小
-        self.num_classes = num_classes # 設置類的數量
-        self.categories = ["face"] # 訓練資料放置的資料夾名稱
-        self.data, self.labels = self.load_data() # 把所有訓練圖片放入data,label放入labels
+
+class CustomDataset2(Dataset):
+    def __init__(self, data_dir, img_size, num_classes):
+        self.data_dir = data_dir
+        self.img_size = img_size
+        self.num_classes = num_classes
+        self.categories = ["face"]
+        self.data, self.labels = self.load_data()
 
     def load_data(self):
         data = []
         labels = []
-        for category in self.categories: # 跑遍所有類
-            path = os.path.join(self.data_dir, category) # 把路徑加起來，得到各類資料夾的路徑
-            label = self.categories.index(category) # 找出category在self.categories中的索引，ex:貓是0,狗是1
-            for img_name in os.listdir(path): # 跑遍所有圖片
-                img_path = os.path.join(path, img_name) # 獲取圖片的路徑
-                print("Loading:", img_path)  # 為了除錯而印出圖像路徑
-                img = cv2.imread(img_path) # 讀取圖片
-                if img is None:
+        for category in self.categories:
+            category_path = os.path.join(self.data_dir, category)
+            # 找出category在self.categories中的索引，ex:貓是0,狗是1,......
+            label = self.categories.index(category)
+            for img_name in os.listdir(category_path):
+                img_path = os.path.join(category_path, img_name)
+                img = cv2.imread(img_path)
+                if img is None:  # 檢查是否為非圖片文件，是則跳過
                     print("Error loading:", img_path)
-                    continue  # 如果讀取失敗，則跳過該圖像
-                img = cv2.resize(img, (self.img_size, self.img_size)) #將所有圖片都縮放成相同大小
-                data.append(img) # 把圖片放進去
-                labels.append(label) # 把label放進去
+                    continue
+                img = cv2.resize(img, (self.img_size, self.img_size))
+                data.append(img)  # 把圖片放進去
+                labels.append(label)  # 把label放進去
 
         data = np.array(data)
         labels = np.array(labels)
         return data, labels
 
-    def __len__(self): # 獲取資料量
+    def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
         """
-        根據索引取得資料集中的一個樣本。
-
-        :param index: 樣本的索引。
-        :return: 返回樣本的圖片和標籤。
+        Args:
+            index (int): 樣本的索引。
+            
+        Returns: 返回樣本的圖片和標籤。
         """
         img = self.data[index]  # 取得圖片資料
         label = self.labels[index]  # 取得標籤資料
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # OpenCV讀取的圖片是BGR順序，轉換為RGB順序
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = transforms.ToTensor()(img)  # 將圖片轉換成PyTorch的Tensor格式
         label = torch.tensor(label, dtype=torch.long)  # 將標籤資料轉換為Long型態
         return img, label
-    
 
 
 def weights_init(m):
@@ -125,8 +121,9 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
+
 class Generator3(nn.Module):
-    def __init__(self,ngpu):
+    def __init__(self, ngpu):
         super(Generator3, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
@@ -153,11 +150,12 @@ class Generator3(nn.Module):
         )
 
     def forward(self, input):
-        #print(self.main(input).size())
         return self.main(input)
 
+
 class Discriminator2(nn.Module):
-    def __init__(self, ngpu, input_shape = (3,image_size,image_size), num_classes = 1):
+    def __init__(self, ngpu,
+                 input_shape=(3, image_size, image_size), num_classes=1):
         super(Discriminator2, self).__init__()
         self.ngpu = ngpu
         self.conv_layers = nn.Sequential(
@@ -182,23 +180,24 @@ class Discriminator2(nn.Module):
             nn.ReLU(),
             nn.Linear(64, num_classes),
             nn.Sigmoid()
-            #nn.Softmax(dim=1)
+            # nn.Softmax(dim=1)
         )
 
     def forward(self, x):
         x = self.conv_layers(x)
         x = self.fc_layers(x)
         x = x.unsqueeze(-1).unsqueeze(-1)
-        #print(x.size())
+        # print(x.size())
         return x
-        
+
+
 class Generator(nn.Module):
     def __init__(self, ngpu):
         super(Generator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. ``(ngf*8) x 4 x 4``
@@ -206,21 +205,22 @@ class Generator(nn.Module):
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. ``(ngf*4) x 8 x 8``
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. ``(ngf*2) x 16 x 16``
-            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. ``(ngf) x 32 x 32``
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. ``(nc) x 64 x 64``
         )
 
     def forward(self, input):
         return self.main(input)
+
 
 class Discriminator(nn.Module):
     def __init__(self, ngpu):
@@ -250,10 +250,11 @@ class Discriminator(nn.Module):
     def forward(self, input):
         return self.main(input)
 
+
 def show_images(images, epoch):
     sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
     print(sqrtn)
-    plt.figure(figsize=(10,10))  # 可以根据需要调整大小
+    plt.figure(figsize=(10, 10))  # 可以根据需要调整大小
     for index, image in enumerate(images):
         plt.subplot(sqrtn, sqrtn, index + 1)
         # 转换图像格式从[channels, height, width]到[height, width, channels]
@@ -264,10 +265,10 @@ def show_images(images, epoch):
         plt.axis('off')  # 关闭坐标轴
     plt.savefig("Generator_epoch_{}.png".format(epoch))
     plt.show()
-    
-    
+
+
 transform = transforms.Compose([
-    transforms.Resize((image_size, image_size)), 
+    transforms.Resize((image_size, image_size)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
@@ -277,9 +278,11 @@ dataset = CustomDataset2(dataroot, image_size, 1)
 
 #dataset = dset.ImageFolder(root=dataroot,transform=transforms.Compose([transforms.Resize(image_size), transforms.CenterCrop(image_size),transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]))
 # Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,shuffle=True)
+dataloader = torch.utils.data.DataLoader(
+    dataset, batch_size=batch_size, shuffle=True)
 
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+device = torch.device("cuda:0" if (
+    torch.cuda.is_available() and ngpu > 0) else "cpu")
 
 # Create the generator
 netG = Generator3(ngpu).to(device)
@@ -329,36 +332,37 @@ iters = 0
 print("Starting Training Loop...")
 # For each epoch
 for epoch in range(num_epochs):
-    #print("0")
+    # print("0")
     # For each batch in the dataloader
-    for i, data in enumerate(dataloader,0):
-        #print("00")
+    for i, data in enumerate(dataloader, 0):
+        # print("00")
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
-        ## Train with all-real batch
+        # Train with all-real batch
         netD.zero_grad()
-        #print("1")
+        # print("1")
         # Format batch
         real_cpu = data[0].to(device)
         b_size = real_cpu.size(0)
-        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+        label = torch.full((b_size,), real_label,
+                           dtype=torch.float, device=device)
         # Forward pass real batch through D
-        #print("2")
+        # print("2")
         output = netD(real_cpu).view(-1)
-        #print("2.5")
-        
+        # print("2.5")
+
         # Calculate loss on all-real batch
-        #print(output.size(),label.size())
+        # print(output.size(),label.size())
         errD_real = criterion(output, label)
-        #print("2.55")
+        # print("2.55")
         # Calculate gradients for D in backward pass
         errD_real.backward()
-        #print("2.6")
+        # print("2.6")
         D_x = output.mean().item()
-        #print("3")
+        # print("3")
 
-        ## Train with all-fake batch
+        # Train with all-fake batch
         # Generate batch of latent vectors
         noise = torch.randn(b_size, nz, 1, 1, device=device)
         # Generate fake image batch with G
@@ -366,7 +370,7 @@ for epoch in range(num_epochs):
         label.fill_(fake_label)
         # Classify all fake batch with D
         output = netD(fake.detach()).view(-1)
-        #print("4")
+        # print("4")
         # Calculate D's loss on the all-fake batch
         errD_fake = criterion(output, label)
         # Calculate the gradients for this batch, accumulated (summed) with previous gradients
@@ -376,7 +380,7 @@ for epoch in range(num_epochs):
         errD = errD_real + errD_fake
         # Update D
         optimizerD.step()
-        #print("5")
+        # print("5")
 
         ############################
         # (2) Update G network: maximize log(D(G(z)))
@@ -392,7 +396,7 @@ for epoch in range(num_epochs):
         D_G_z2 = output.mean().item()
         # Update G
         optimizerG.step()
-        #print("6")
+        # print("6")
 
         # Output training stats
         if i % 50 == 0:
@@ -405,10 +409,11 @@ for epoch in range(num_epochs):
         D_losses.append(errD.item())
 
         # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+        if (iters % 500 == 0) or \
+                ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
             with torch.no_grad():
                 fake = netG(fixed_noise).detach().cpu()
-                show_images(fake[:16],epoch)
+                show_images(fake[:16], epoch)
                 plt.show()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
